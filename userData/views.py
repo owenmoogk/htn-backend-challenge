05c4cae2cf78aeab6f294469296b140ff6d60658
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2023-02-14 18:08:17 -0500

    fixed issue when changing user data

--- userData/views.py
+++ userData/views.py
@@ -11,10 +11,10 @@ from .models import *
 def serializeSkills(skills):
   returnData = {}
   for skill in skills:
-    print(skill)
     returnData[skill['name']] = skill['rating']
   return returnData
 
+# return all of the users in json
 class UserList(APIView):
 
   def get(self, request):
@@ -25,11 +25,13 @@ class UserList(APIView):
 
 class UserInfo(APIView):
 
+  # return a single users data in a json
   def get(self, request, user_id):
     user = User.objects.get(id = user_id)
     data = userSerializer(user)
     return Response(data, status=status.HTTP_200_OK)
 
+  # update a user's data
   def put(self, request, user_id):
 
     data = request.data
@@ -37,14 +39,29 @@ class UserInfo(APIView):
     user = User.objects.get(id = user_id)
 
     for key in data:
+      
+      # if the data to be updated is in the skills section
       if key == 'skills':
         updatedSkills = serializeSkills(data[key])
+
+        # loop through skills and check if they need to be updated
         for skillObj in user.skills.all():
           if skillObj.name in updatedSkills:
             skillObj.rating = updatedSkills[skillObj.name]
             skillObj.save()
+            updatedSkills.pop(skillObj.name)
+        
+        # for the skills that don't need to be updated
+        for skill in updatedSkills:
+          newSkill = Skill(name=skill, rating=updatedSkills[skill])
+          newSkill.save()
+          user.skills.add(newSkill)
+
+      # otherwise we just update the current user data
       else:
         setattr(user, key, data[key])
+    
+    # save the user model
     user.save()
 
     return Response(userSerializer(user), status=status.HTTP_200_OK)
