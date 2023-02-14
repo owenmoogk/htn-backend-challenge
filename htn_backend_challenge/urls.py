f34091740fc9fd6c375ff4b7c7d617f97dd53547
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2023-02-14 18:10:07 -0500

    update database

--- htn_backend_challenge/urls.py
+++ htn_backend_challenge/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# register the admin url, redirect to userData urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('userData.urls'))
