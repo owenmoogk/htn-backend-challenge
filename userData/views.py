1a1b793c6977920da43b3a950ce46429c3966d8b
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2023-02-14 14:45:38 -0500

    skills aggregate endpoint

--- userData/views.py
+++ userData/views.py
@@ -26,10 +26,10 @@ class UserList(APIView):
 class UserInfo(APIView):
 
   def get(self, request, user_id):
-    print(user_id)
     user = User.objects.get(id = user_id)
     data = userSerializer(user)
     return Response(data, status=status.HTTP_200_OK)
+
   def put(self, request, user_id):
 
     data = request.data
@@ -49,3 +49,29 @@ class UserInfo(APIView):
 
     return Response(userSerializer(user), status=status.HTTP_200_OK)
 
+class Skills(APIView):
+  
+  def get(self, request):
+    try:
+      minFreq = int(request.GET['min_frequency'])
+    except:
+      minFreq = 0
+    try:
+      maxFreq = int(request.GET['max_frequency'])
+    except:
+      maxFreq = float('inf')
+
+    skills = Skill.objects.all()
+    aggregate = {}
+    for skillObj in skills:
+      if skillObj.name in aggregate:
+        aggregate[skillObj.name] += 1
+      else:
+        aggregate[skillObj.name] = 0
+
+    returnData = {}
+    for key in aggregate:
+      if aggregate[key] <= maxFreq and aggregate[key] >= minFreq:
+        returnData[key] = aggregate[key]
+
+    return Response(returnData, status=status.HTTP_200_OK)
