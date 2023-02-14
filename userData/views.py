c772960d3e68c0d394a5b8189e36d93f4dc1ec61
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2023-02-14 14:45:21 -0500

    modify user endpoint

--- userData/views.py
+++ userData/views.py
@@ -7,6 +7,13 @@ from django.core import serializers
 import json
 from .models import *
 
+# the function turns the slightly messier skill data into a rating dictionary's
+def serializeSkills(skills):
+  returnData = {}
+  for skill in skills:
+    print(skill)
+    returnData[skill['name']] = skill['rating']
+  return returnData
 
 class UserList(APIView):
 
@@ -20,6 +27,25 @@ class UserInfo(APIView):
 
   def get(self, request, user_id):
     print(user_id)
-    user = User.objects.get(pk = 1)
+    user = User.objects.get(id = user_id)
     data = userSerializer(user)
     return Response(data, status=status.HTTP_200_OK)
+  def put(self, request, user_id):
+
+    data = request.data
+
+    user = User.objects.get(id = user_id)
+
+    for key in data:
+      if key == 'skills':
+        updatedSkills = serializeSkills(data[key])
+        for skillObj in user.skills.all():
+          if skillObj.name in updatedSkills:
+            skillObj.rating = updatedSkills[skillObj.name]
+            skillObj.save()
+      else:
+        setattr(user, key, data[key])
+    user.save()
+
+    return Response(userSerializer(user), status=status.HTTP_200_OK)
+
