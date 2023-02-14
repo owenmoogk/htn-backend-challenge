595f81d84616de4faf19f2d919614688ad518ad2
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2023-02-14 14:44:38 -0500

    update urls

--- userData/urls.py
+++ userData/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', UserList.as_view()),
-    path('<int:user_id>/', UserInfo.as_view())
+    path('users/', UserList.as_view()),
+    path('user/<int:user_id>/', UserInfo.as_view()),
+    path(r'skills/', Skills.as_view())
 ]
